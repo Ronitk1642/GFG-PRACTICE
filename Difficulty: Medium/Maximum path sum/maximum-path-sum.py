d40f4134a2ff7fabeb6cd151972e9d30b3c78c06
RONIT KUMAR <ronit1642@gmail.com>
2025-10-11 23:38:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Maximum path sum/maximum-path-sum.py
@@ -0,0 +1,23 @@
+class Solution:
+    def __init__(self):
+        self.res = -sys.maxsize
+        
+    def dfs(self, root):
+        r1 = r2 = 0
+        if root.left:
+            r1 = self.dfs(root.left)
+
+        if root.right:
+            r2 = self.dfs(root.right)
+        
+        self.res = max(self.res, r1 + root.data, r2 + root.data, root.data, r1 + r2 + root.data)
+        
+        res = max(r1, r2) + root.data
+
+        return 0 if res < 0 else res
+
+        
+    def findMaxSum(self, root): 
+        # code here
+        self.dfs(root)
+        return self.res
