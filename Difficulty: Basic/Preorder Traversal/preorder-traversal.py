c613e4c7c4f21153412feaa39f0510ccacdc89c6
RONIT KUMAR <ronit1642@gmail.com>
2026-07-23 21:09:47 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Preorder Traversal/preorder-traversal.py
@@ -0,0 +1,40 @@
+'''
+# Node Class:
+class Node:
+    def __init__(self,val):
+        self.data = val
+        self.left = None
+        self.right = None
+'''
+# Morris Pre-order Traversal
+
+class Solution:
+    def preOrder(self, root):
+        
+        curr = root
+        res = []
+        
+        while curr != None:
+            
+            if curr.left:
+                
+                pred = curr.left
+                while pred.right != None and pred.right != curr:
+                    pred = pred.right
+                    
+                if pred.right is None:
+                    res.append(curr.data)
+                    pred.right = curr
+                    curr = curr.left
+                
+                else:
+                    pred.right = None
+                    curr = curr.right
+            
+            else:
+                res.append(curr.data)
+                curr = curr.right
+                
+                
+        return res
+        
